d0c5e39858cb2f18ef62961b794ff5617b281d3f
eak65 <keiserea@gmail.com>
2020-02-15 14:12:40 -0500

    Initial Commit

---
+++ DockDeposit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DockDeposit
+//
+//  Created by Ethan Keiser on 2/15/20.
+//  Copyright © 2020 Ethan Keiser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
